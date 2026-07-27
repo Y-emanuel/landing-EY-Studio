dad2bd59ee230afa3f1d43958de0e74f6d4c17c7
Y-emanuel <brianybalo70@gmail.com>
2026-07-27 10:19:38 -0300

    perf: unificar todo el código en un solo HTML para eliminar solicitudes de red

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), viteSingleFile()],
+  build: {
+    // Desactiva la división de CSS para que todo vaya al HTML
+    cssCodeSplit: false,
+    // Opcional: si no quieres que se genere un archivo de mapa fuente
+    sourcemap: false,
+  },
+});

696ea13078da154998ba91fc33befa1d7bc131a8
Y-emanuel <brianybalo70@gmail.com>
2026-07-27 10:29:58 -0300

    actualizacion vie.confing.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,10 @@ import { viteSingleFile } from 'vite-plugin-singlefile';
 export default defineConfig({
   plugins: [react(), viteSingleFile()],
   build: {
+    
     // Desactiva la división de CSS para que todo vaya al HTML
     cssCodeSplit: false,
     // Opcional: si no quieres que se genere un archivo de mapa fuente
-    sourcemap: false,
+    sourcemap: true,
   },
 });
